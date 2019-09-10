745da3e8f5fab22e13ddf8fa5a4879460af3d115
Connor Shlatz <cshlatz@gmail.com>
2019-09-10 15:57:15 -0400

    Added proper sprite flipping

--- src/Components/RenderComponent.cs
+++ src/Components/RenderComponent.cs
@@ -3,6 +3,13 @@ using Microsoft.Xna.Framework.Graphics;
 namespace Kazaam.Objects {
   public class RenderComponent {
     public Texture2D Texture {get; set;}  
-    public SpriteEffects effects;
+    public SpriteEffects Effects {get; set;}
+    public int Scale {get; set;}
+
+    public RenderComponent(Texture2D texture, SpriteEffects effects = SpriteEffects.None, int scale = 1) {
+      Texture = texture;
+      Effects = effects;
+      Scale = scale;
+    }
   }
 }
