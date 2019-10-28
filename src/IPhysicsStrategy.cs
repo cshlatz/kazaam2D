046eaa7fdfd42ab7d7c9c5f58e726c074f5d14a0
Connor Shlatz <cshlatz@gmail.com>
2019-10-28 13:34:58 -0400

    Added physics strategy interface

---
+++ src/IPhysicsStrategy.cs
@@ -0,0 +1,12 @@
+﻿using Kazaam.Objects;
+using Microsoft.Xna.Framework;
+
+namespace Kazaam.Universe {
+    /// <summary>
+    /// Enforces a pattern strategy design pattern for processing physics
+    /// </summary>
+    public interface IPhysicsStrategy {
+        void Update(GameTime gameTime);
+        Body PhysicsBody {get; set;}
+    }
+}
