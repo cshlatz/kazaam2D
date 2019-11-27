03d830799582f6faaf62cc551d1d26e2f5904f20
Connor Shlatz <cshlatz@gmail.com>
2019-11-27 15:21:07 -0500

    Map tabwidth 2 -> 4

--- src/Map.cs
+++ src/Map.cs
@@ -7,77 +7,64 @@ using MonoGame.Extended.Tiled;
 using MonoGame.Extended.Tiled.Renderers;
 using System.Collections.Generic;
 
-namespace Kazaam.Universe
-{
+namespace Kazaam.Universe {
     /// <summary>
     /// A representation of the engine world.
     /// </summary>
-    public class Map : Objects.IDrawable
-    {
-      public TiledMap map;
+    public class Map : Objects.IDrawable {
+        public TiledMap map;
 
-      private TiledMapRenderer mapRenderer;
+        private TiledMapRenderer mapRenderer;
 
-      public int width;
-      public int height;
-      public int tileWidth;
-      public int tileHeight;
-      public float offsetX;
-      public float offsetY;
+        public int width;
+        public int height;
+        public int tileWidth;
+        public int tileHeight;
+        public float offsetX;
+        public float offsetY;
 
-      public Scene scene;
+        public Scene scene;
 
-      // Backgrounds
-      public List<Background> Backgrounds { get; set; }
+        // Backgrounds
+        public List<Background> Backgrounds { get; set; }
 
-      public Vector2 StartingPosition;
+        public Vector2 StartingPosition;
 
-      public HumperWorld HumperWorld {get; set;}
+        public HumperWorld HumperWorld {get; set;}
 
-      public Map(TiledMap map, int tileWidth, int tileHeight, GraphicsDevice gd) {
-        // Initialize the background list
-        Backgrounds = new List<Background>();
-        this.map = map;
-        this.mapRenderer = new TiledMapRenderer(gd, map);
-        width = map.Width * tileWidth;
-        height = map.Height * tileHeight;
-        this.tileWidth = tileWidth;
-        this.tileHeight = tileHeight;
-      }
-
-      public void Update(GameTime gameTime) {
-        mapRenderer.Update(gameTime);
-        foreach (Background bg in Backgrounds) {
-          bg.Update(gameTime);
+        public Map(TiledMap map, int tileWidth, int tileHeight, GraphicsDevice gd) {
+            // Initialize the background list
+            Backgrounds = new List<Background>();
+            this.map = map;
+            this.mapRenderer = new TiledMapRenderer(gd, map);
+            width = map.Width * tileWidth;
+            height = map.Height * tileHeight;
+            this.tileWidth = tileWidth;
+            this.tileHeight = tileHeight;
         }
-      }
 
-      public void Draw(SpriteBatch sb, Scene scene) {
-        var newMatrix = scene.CameraManager.View * Matrix.CreateTranslation((offsetX * scene.CameraManager.Zoom), (offsetY * scene.CameraManager.Zoom), 0);
+        public void Update(GameTime gameTime) {
+            mapRenderer.Update(gameTime);
+            foreach (Background bg in Backgrounds) {
+                bg.Update(gameTime);
+            }
+        }
 
-        // Draw background
-        /*
-        foreach (Background bg in Backgrounds) {
-            sb.Begin(samplerState: bg?.SamplerState ?? SamplerState.LinearWrap, sortMode: SpriteSortMode.FrontToBack); // Draw the background
-            sb.Draw(bg.Texture, new Vector2(scene.CameraManager.Viewport.X, scene.CameraManager.Viewport.Y), bg.Rectangle(scene.CameraManager.Viewport), Color.White, 0, Vector2.Zero, bg.Zoom, SpriteEffects.None, bg.LayerDepth);
+        public void Draw(SpriteBatch sb, Scene scene) {
+            var newMatrix = scene.CameraManager.View * Matrix.CreateTranslation((offsetX * scene.CameraManager.Zoom), (offsetY * scene.CameraManager.Zoom), 0);
+            sb.Begin(transformMatrix: newMatrix, samplerState: SamplerState.PointClamp); // Draw the map
+            mapRenderer.Draw(newMatrix);
             sb.End();
         }
-        */
 
-        // SamplerState.PointClamp prevents gaps between the tiles while rendering
-        sb.Begin(transformMatrix: newMatrix, samplerState: SamplerState.PointClamp); // Draw the map
-        mapRenderer.Draw(newMatrix);
-        sb.End();
-      }
-
-      public void AddBackground(Background bg) {
-        Backgrounds.Add(bg);
-        var entity = scene.CreateEntity();
-        var bgBody = new Body();
-        bgBody.Position = new Vector2(0, 0);
-        entity.Attach(bgBody);
-        entity.Attach(bg);
-        entity.Attach(new RenderComponent(bg.Texture));
-      }
+        public void AddBackground(Background bg) {
+            Backgrounds.Add(bg);
+            var entity = scene.CreateEntity();
+            var bgBody = new Body();
+            bgBody.Position = new Vector2(0, 0);
+            entity.Attach(bgBody);
+            entity.Attach(bg);
+            entity.Attach(new RenderComponent(bg.Texture));
+        }
     } 
 }
