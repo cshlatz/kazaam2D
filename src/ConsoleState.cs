af8801d0dc0173ea5257cab57925cbb50e3ac976
Connor Shlatz <cshlatz@gmail.com>
2020-04-19 12:27:53 -0400

    Tilde removes ConsoleState from State stack

--- src/ConsoleState.cs
+++ src/ConsoleState.cs
@@ -1,4 +1,5 @@
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
 
 namespace Kazaam {
     public class ConsoleState : GameState {
@@ -11,7 +12,10 @@ namespace Kazaam {
         }
 
         public override void Update(GameTime gameTime) {
-
+            KeyboardState kb = Keyboard.GetState();
+            if (kb.IsKeyDown(Keys.OemTilde)) {
+                game.Scene.States.Pop();
+            }
         }
 
         public override void Draw(GameTime gameTime) {
