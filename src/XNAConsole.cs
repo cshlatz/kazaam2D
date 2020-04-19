050f79392a9e94df5352339a903d6b3e602bcf71
Connor Shlatz <cshlatz@gmail.com>
2020-04-19 13:34:14 -0400

    Finalized beta console, with working quit command

--- src/XNAConsole.cs
+++ src/XNAConsole.cs
@@ -6,12 +6,39 @@ using Myra.Graphics2D.UI;
 
 namespace Kazaam {
     public class XNAConsole : Gui {
+
+        private TextBox InputTextBox {
+            get {
+                return (TextBox)Project.Root.FindWidgetById("console-input");
+            }
+        }
+
+        private TextBox HistoryTextBox {
+            get {
+                return (TextBox)Project.Root.FindWidgetById("console-history");
+            }
+        }
+
         public XNAConsole(XNAGame game) : base(game) {
             Assembly assembly = Assembly.GetExecutingAssembly();
             StreamReader reader = new StreamReader(assembly.GetManifestResourceStream("kazaam.res.gui.xnaconsole.xml"));
             string xmlgui = reader.ReadToEnd();
             LoadFromXML(xmlgui);
             Toggle();
+        }
+
+        public string ReturnConsoleCommand() {
+            return InputTextBox.Text;
+        }
+
+        public void SetInputText(string input) {
+            var inputBox = InputTextBox;
+            inputBox.Text = input;
+        }
+
+        public void SetHistoryText(string text) {
+            var historyBox = HistoryTextBox;
+            historyBox.Text = historyBox.Text + text + Environment.NewLine;
         }
     }
 }
